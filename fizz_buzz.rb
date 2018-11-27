a76d5241c7f0a6af3caee58b47557b7e356df927
Victoria Guerrero <victoria@tenforwardconsulting.com>
2018-11-27 13:10:21 -0600

    Convert tabs to spaces

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,15 +2,15 @@ my_array=(1..30)
 
 my_array.each do |n|
 
-	if n % 3 == 0 && n % 5 == 0
-		puts "Fizz Buzz"
+  if n % 3 == 0 && n % 5 == 0
+    puts "Fizz Buzz"
 
-		elsif n % 3 == 0
-			puts "Fizz"
+    elsif n % 3 == 0
+      puts "Fizz"
 
-		elsif n % 5 == 0
-			puts "Buzz"
-	else
-		puts "#{n}"  
-	end
+    elsif n % 5 == 0
+      puts "Buzz"
+  else
+    puts "#{n}"
+  end
 end
