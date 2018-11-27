62e3f95f70a9713526266f1f1a608cab0f0ef7ca
Victoria Guerrero <victoria@tenforwardconsulting.com>
2018-11-27 16:34:06 -0600

    Make vending machine request additional payment when initial payment is less than price

--- vending_challenge3.rb
+++ vending_challenge3.rb
@@ -2,15 +2,15 @@ require "./vending_machine"
 require "pry"
 
 items = {
-  001 => {name: "Coke", price: 25 },
-  002 => {name: "Pepsi", price: 35 },
-  003 => {name: "Sprite", price: 45}
+  1 => {name: "Coke", price: 25 },
+  2 => {name: "Pepsi", price: 35 },
+  3 => {name: "Sprite", price: 45}
   }
 
 v = VendingMachine.new(items)
 
 puts "This vending machine sells Coke, Pepsi, and Sprite."
-puts "Select 001 for Coke, 002 for Pepsi, or 003 for Sprite."
+puts "Select 1 for Coke, 2 for Pepsi, or 3 for Sprite."
 code = gets.chomp
 
 selection_valid = v.selection_valid?(code.to_i)
@@ -27,7 +27,7 @@ puts "Do you want to make this your selection? Y or N."
 answer = gets.chomp
 
 until answer == "Y" || answer == "y"
-  puts "Select 001 for Coke, 002 for Pepsi, or 003 for Sprite."
+  puts "Select 1 for Coke, 2 for Pepsi, or 3 for Sprite."
   code = gets.chomp
 
   selection_valid = v.selection_valid?(code.to_i)
@@ -37,31 +37,38 @@ until answer == "Y" || answer == "y"
     code = gets.chomp
     selection_valid = v.selection_valid?(code.to_i)
   end
-
+  v.show_price(code.to_i)
   puts "Do you want to make this your selection? Y or N."
   answer = gets.chomp
 end
 
 v.current_selection(code.to_i)
 
-payment = gets.chomp
+payment = gets.chomp.to_i
 v.accept_payment(payment.to_i)
 
-puts "You paid #{payment} cents. Press Y to vend, press N to refund."
+item = items
+price = item[code.to_i][:price]
+
+if payment >= price
+  puts "You paid #{payment.to_i} cents. Press Y to vend, press N to refund."
+else
+  puts "You paid #{payment} cents, You owe #{(price) - (payment)}. Submit the rest of your payment."
+
+  first_payment = payment
+  additional_payment = gets.chomp.to_i
+  final_payment = (first_payment + additional_payment).to_i
+
+  if final_payment == price
+    puts "You paid #{final_payment} cents. Press Y to vend, press N to refund."
+  else
+    puts "You paid #{final_payment} cents, You owe #{(price) - (final_payment)}. Submit the rest of your payment."
+  end
+end
 answer = gets.chomp
 
-paid_in_full = v.paid_in_full?(payment.to_i)
-
-until paid_in_full
-  puts "Submit the rest of your payment"
-  additional_payment = gets.chomp.to_i
-  first_payment = payment.to_i
-  binding.pry
-  break if additional_payment + first_payment == price
-end
-
-if answer == "N" || answer == "n"
-  puts "Your order has been canceled. Your payment of #{payment} cents has been refunded. Goodbye!"
-else
+if answer == "Y" || answer == "y"
   v.vend(code.to_i, payment.to_i)
+else
+  puts "Your order has been canceled. Your payment of #{payment} cents has been refunded. Goodbye!"
 end
